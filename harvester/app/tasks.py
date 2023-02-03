cf46249a1c994291d29f5debfbf50bfa2eab1b19
geb789 <56951253+geb789@users.noreply.github.com>
2023-02-03 14:17:17 +0800

    fix block update to database
    fix block update to database

--- harvester/app/tasks.py
+++ harvester/app/tasks.py
@@ -114,7 +114,7 @@ def accumulate_block_recursive(self, block_hash, end_block_hash=None):
 
     try:
 
-        for nr in range(0, 100):
+        for nr in range(0, 20):
             if not block or block.id > 0:
                 # Process block
                 block = harvester.add_block(block_hash)
