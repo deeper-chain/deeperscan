5be5e65334da3c42db01757809620c9f3ba01082
geb789 <56951253+geb789@users.noreply.github.com>
2023-02-04 10:13:31 +0800

    add log

--- harvester/app/processors/block.py
+++ harvester/app/processors/block.py
@@ -221,9 +221,8 @@ class AccountBlockProcessor(BlockProcessor):
         self.block.count_accounts = self.block.count_accounts_new - self.block.count_accounts_reaped
 
     def sequencing_hook(self, db_session, parent_block_data, parent_sequenced_block_data):
-        if settings.DEEPER_DEBUG:
-            print("DEEPER--->>> AccountBlockProcessor sequencing_hook self.block.id = {}".format(self.block.id))
-
+        print("test DEEPER--->>> AccountBlockProcessor sequencing_hook self.block.id = {}".format(self))
+        
         for account_audit in AccountAudit.query(db_session).filter_by(block_id=self.block.id).order_by('event_idx'):
             if settings.DEEPER_DEBUG:
                 print("DEEPER--->>> AccountBlockProcessor sequencing_hook account_audit.account_id = {}".format(account_audit.account_id))
