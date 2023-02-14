9aba3aaa6dd817c63051a2693ad4fc7a4593b585
geb789 <56951253+geb789@users.noreply.github.com>
2023-02-14 10:46:43 +0800

    add log for cpu too high

--- explorer-api/app/resources/polkascan.py
+++ explorer-api/app/resources/polkascan.py
@@ -1528,6 +1528,13 @@ class TransactionResource2(BaseResource):
         result = self.session.execute(sql, params)
 
         conditions = []
+        if len(result) > 1000:
+            print('!testDEBUG ---> account')
+            print(addr)
+            print(from_addr)
+            print(to_addr)
+            print(sum_option)
+               
         for row in result:
             # print("result:", row)
             row = list(row)
