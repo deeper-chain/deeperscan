2e44b78bcc7642387a2ee4c1c2610531a71c95af
geb789 <56951253+geb789@users.noreply.github.com>
2023-02-14 14:49:13 +0800

    Update polkascan.py

--- explorer-api/app/resources/polkascan.py
+++ explorer-api/app/resources/polkascan.py
@@ -1525,7 +1525,6 @@ class TransactionResource2(BaseResource):
 
         data = []
         sum_amount = 0
-        sql += ' ORDER BY block_id DESC limit 600'
         result = self.session.execute(sql, params)
 
         conditions = []
